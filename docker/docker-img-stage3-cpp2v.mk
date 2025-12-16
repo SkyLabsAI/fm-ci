d39ffc9f925e83eaed0adfd53582d3b344b0503d
Paolo G. Giarrusso <paolo@skylabs-ai.com>
2025-12-16 03:49:31 +0100

    Docker images: do not install stdlib specs
    Fix #134.

--- docker/docker-img-stage3-cpp2v.mk
+++ docker/docker-img-stage3-cpp2v.mk
@@ -1,7 +1,11 @@
 .PHONY: fm-$(BR_FMDEPS_VERSION)-stage3-release
 fm-$(BR_FMDEPS_VERSION)-stage3-release: fm-$(BR_FMDEPS_VERSION)-stage2-llvm-$(LLVM_MAIN_VERSION) prepare-fm-release
 	$(call opam-img-target,\
-	grep -E "(rocq-skylabs-cpp2v|rocq-skylabs-cpp-stdlib|rocq-brick-libstdcpp|rocq-skylabs-scaffold)")
+	grep -E "(rocq-skylabs-cpp2v|rocq-skylabs-scaffold)")
+
+# We exclude `rocq-skylabs-cpp-stdlib|rocq-brick-libstdcpp`: they shouldn't be
+# installed when developing the stdlib.
+# See https://github.com/SkyLabsAI/skylabs-fm/issues/134
 
 DOCKER_BUILD_TARGETS += fm-$(BR_FMDEPS_VERSION)-stage3-release
 DOCKER_PUSH_TARGETS += push-fm-$(BR_FMDEPS_VERSION)-stage3-release
