bd592a69c3a25e1de0ce35ddbcf991b562964995
Paolo G. Giarrusso <paolo@skylabs-ai.com>
2025-12-21 18:05:28 +0100

    Move docker/ folder away into `ci` repo

--- docker/docker-img-stage2-auto.mk
+++
@@ -1,8 +0,0 @@
-.PHONY: fm-$(BR_FMDEPS_VERSION)-stage2
-fm-$(BR_FMDEPS_VERSION)-stage2: fm-$(BR_FMDEPS_VERSION)-stage1 prepare-fm-release
-	$(call opam-img-target,\
-	grep -E "/fmdeps" | \
-	grep -E -v "(/fmdeps/(skylabs-fm|vendored)|rocq-skylabs-cpp2v|rocq-skylabs-cpp-stdlib|rocq-brick-libstdcpp|rocq-skylabs-scaffold)")
-
-DOCKER_BUILD_TARGETS += fm-$(BR_FMDEPS_VERSION)-stage2
-DOCKER_PUSH_TARGETS += push-fm-$(BR_FMDEPS_VERSION)-stage2
